4ed3fc4fc252a64a6d5df483953b80a1506bd744
Aaron Ludwig <107589333+ahaaron05@users.noreply.github.com>
2024-12-26 23:52:31 -0700

    yay more stuff

---
+++ Chapter 1/Readings/primality.rkt
@@ -0,0 +1,50 @@
+#lang scheme
+
+(define (square x)
+  (* x x))
+
+(define (even? x)
+  (= (remainder x 2) 0))
+
+
+; O(sqrt(n)) algo version
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+; check if has a remainder > 0
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+;(prime? 5)
+
+
+; O(log(n)) algo version (compute exponential of a number modulo another number).
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+         (else (remainder (* base (expmod base (- exp 1) m))
+                          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 7 1000)
+
+
